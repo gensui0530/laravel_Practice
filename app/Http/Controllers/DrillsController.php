cff38745a29ad0ad47cf3fd3e22aabf24f0cff02
gensui <gensui0530@gmail.com>
2020-07-26 19:51:52 +0900

    Lravel+Vueでのアプリケーション npm run dev のコマンドでコンパイルを実施

---
+++ app/Http/Controllers/DrillsController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Drill;
+
+class DrillsController extends Controller
+{
+
+    public function index()
+    {
+        $drills = Drill::all();
+        return view('drills.index', ['drills' => $drills]);
+    }
+
+    public function new()
+    {
+        return view('drills.new');
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'category_name' => 'required|string|max:255',
+            'problem0' => 'required|string|max:255',
+            'problem1' => 'string|nullable|max:255',
+            'problem2' => 'string|nullable|max:255',
+            'problem3' => 'string|nullable|max:255',
+            'problem4' => 'string|nullable|max:255',
+            'problem5' => 'string|nullable|max:255',
+            'problem6' => 'string|nullable|max:255',
+            'problem7' => 'string|nullable|max:255',
+            'problem8' => 'string|nullable|max:255',
+            'problem9' => 'string|nullable|max:255',
+        ]);
+
+        //モデルを使って，DBに登録する値をセット
+        $drill = new Drill;
+
+        // １つ１つ入れるか
+        //        $drill->title = $request->title;
+        //        $drill->category_name = $request->category_name;
+        //        $drill->save();
+
+        // fillを使って一気にいれるか
+        // $fillableを使っていないと変なデータが入り込んだ場合に勝手にDBが更新されてしまうので注意！
+        $drill->fill($request->all())->save();
+
+        // リダイレクトする
+        // その時にsessionフラッシュにメッセージを入れる
+        return redirect('/drills/new')->with('flash_message', __('Registered.'));
+    }
+
+    public function show($id)
+    {
+        // GETパラメータが数字かどうかをチェックする
+        if (!ctype_digit($id)) {
+            return redirect('/drills/new')->with('flash_message', __('Invalid operation was performed.'));
+        }
+
+        $drill = Drill::find($id);
+
+        return view('drills.show', ['drill' => $drill]);
+    }
+
+    public function edit($id)
+    {
+        // GETパラメータが数字かどうかをチェックする
+        // 事前にチェックしておくことでDBへの無駄なアクセスが減らせる（WEBサーバーへのアクセスのみで済む）
+        if (!ctype_digit($id)) {
+            return redirect('/drills/new')->with('flash_message', __('Invalid operation was performed.'));
+        }
+
+        $drill = Drill::find($id);
+        return view('drills.edit', ['drill' => $drill]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        // GETパラメータが数字かどうかをチェックする
+        if (!ctype_digit($id)) {
+            return redirect('/drills/new')->with('flash_message', __('Invalid operation was performed.'));
+        }
+
+        $drill = Drill::find($id);
+        $drill->fill($request->all())->save();
+
+        return redirect('/drills')->with('flash_message', __('Registered.'));
+    }
+
+    public function destroy($id)
+    {
+        // GETパラメータが数字かどうかをチェックする
+        if (!ctype_digit($id)) {
+            return redirect('/drills/new')->with('flash_message', __('Invalid operation was performed.'));
+        }
+
+        // $drill = Drill::find($id);
+        // $drill->delete();
+
+        // こう書いた方がスマート
+        Drill::find($id)->delete();
+
+        return redirect('/drills')->with('flash_message', __('Deleted.'));
+    }
+}
---
+++ resources/views/drills/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="app">
+    <!-- デフォルトだとこの中ではvue.jsが有効 -->
+    <!-- example-component はLaravelに入っているサンプルのコンポーネント -->
+    <example-component title="{{ __('Practice').'「'.$drill->title.'」' }}" :drill="{{$drill}}"></example-component>
+    </example-component>
+</div>
+
+@endsection
